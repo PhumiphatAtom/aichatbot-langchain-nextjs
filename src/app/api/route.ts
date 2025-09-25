7aa8eb100a35fbea0f687b23dbdbf5c23d857cf0
Phumiphat <atom_li1@hotmail.com>
2025-09-25 23:36:57 +0700

    finished 01 next api

---
+++ src/app/api/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({ message: "API Running with GET" });
+}
+
+export async function POST() {
+  return NextResponse.json({ message: "API Running with POST" });
+}
+
+export async function PUT() {
+  return NextResponse.json({ message: "API Running with PUT" });
+}
+
+export async function DELETE() {
+  return NextResponse.json({ message: "API Running with DELETE" });
+}
